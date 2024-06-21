22e5660629703037438da906494345551e21cb0d
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-06-21 13:32:02 +0000

    Word on Stateless Functional Component

---
+++ react-rev-v1/src/components/FunctionalGreeting.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function FunctionalGreeting(props) {
+  return (
+    <div>
+        <h1>Hello my name is {props.name}</h1>
+        <h2>I am Stateless Functional Component</h2>
+    </div>
+  )
+}
