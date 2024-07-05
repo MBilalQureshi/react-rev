0d640a4161de953fca0f6ccc44dddbc5f9294ec7
mbilalqureshi <bilalqureshi.epic@gmail.com>
2024-07-05 20:03:45 +0200

    Handle if fetched data is object not an array and handle errors

--- react-rev-v1/src/components/HTTPRequests.js
+++ react-rev-v1/src/components/HTTPRequests.js
@@ -6,22 +6,51 @@ export class HTTPRequests extends Component {
       super(props)
     
       this.state = {
-         posts:[]
+         posts:[],
+         error:null,
       }
     }
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
         .then(response => {
-            console.log(response.data)
-            this.setState({posts: response.data})
+            console.log(response)
+            this.setState({
+                // switch above url BY PROVIDING /1 to see the difference, it will return an object, solution is below after posts:
+                // Put in an array if object is returned, otherwise just return the object as it's already in an array
+                posts: Array.isArray(response.data) ? response.data : [response.data]
+            })
+        })
+        .catch(error => {
+            // make above url incorrect to see the error
+          console.log(error)
+          this.setState({
+                error: error.message,
+          })
         })
     }
   render() {
+    const posts = this.state.posts
     return (
       <div>
-        {/* {this.state.posts.map(post=><div><h1>{post.body}</h1></div>)} */}
-        {/* below is better for now not above, the code is valid tough */}
-        {JSON.stringify(this.state.posts)}
+        {/* {JSON.stringify(this.state.posts)} */}
+        <h2>Posts</h2> 
+        {
+            posts.length ? (
+                posts.map(post => (
+                    <div key={post.id}>
+                        <h2>{post.id} . {post.title}</h2>
+                        <h4>By user id: {post.userId}</h4>
+                        <p>{post.body}</p>
+                        <hr />
+                    </div>
+                )
+                )
+            ):(
+                this.state.error ?
+                (<h4>{this.state.error}</h4>) :
+                (<h4>Loading...</h4>)
+        )
+        }
       </div>
     )
   }
